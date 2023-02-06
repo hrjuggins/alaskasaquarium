f59e5e2f9c2c50d1ca8df462a37bab8d7cc7cba6
hrjuggins <hrjuggins@gmail.com>
2023-02-06 15:28:27 +0000

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -59,6 +59,7 @@ const renderFishes: string[] | string = fishes.map(
        
           <form name="contact" method="POST" data-netlify="true" class="submit-name">
           <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="fish" value="${fish.name}" />
             <input type="text" placeholder="Enter your name" name="name" />
             <button class="send" type="submit">✅</button>
             <button class="exit">❌</button>
